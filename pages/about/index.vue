8faf8078ee15ef2bf5c151485cb71aeb2a85f25a
Кирилл <kirill000e@gmail.com>
2025-01-04 12:12:25 +0500

    Lending2

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,1 +1,210 @@
-<template>about</template>
+<script></script>
+
+<template>
+    <div class="bg-white text-black px-52">
+        <div class="flex items-start px-52">
+            <div class="mb-4 flex flex-col items-center">
+                <img src="~/public/freepik__two-people-.jpeg" alt="img" class="w-52 h-52" />
+                <div class="border border-black rounded-lg mt-20">
+                    <button class="px-4 py-2 text-black  rounded-lg">Заказать звонок</button>
+                </div>
+            </div>
+            <div class="ml-4 flex-1"> 
+                <h1 class="text-xl font-bold mb-2 text-center">СЕМЕЙНЫЕ ДЕЛА И СПОРЫ</h1>
+                <p class="mb-2 px-20 text-center">
+                    имущество, взыскание алиментов, Определение порядка общения с ребенком, места проживания ребенка.
+                </p>
+                <div class="text-container mt-20 px-52">
+                    <p class="narrow-text">
+                        Семейные дела могут быть одними из самых сложных и эмоционально насыщенных процессов в жизни. Мы понимаем, что каждый случай уникален и требует индивидуального подхода. Наша команда профессионалов готова оказать вам поддержку и помочь в решении вопросов, связанных с семейными спорами.
+                    </p>
+                </div>
+                <hr class="border-t border-gray-300 my-4 px-52" />
+            </div>
+        </div>
+
+
+        <div>
+            <div class="flex items-stretch">
+                <div class="mb-4 flex-1 max-w-md">
+                    <h1 class="text-xl font-bold mb-2 text-center">Семейные споры, по которым работают наши адвокаты:</h1>
+                    <p class="mb-2 text-left">
+                        Мы индивидуально подходим к каждому делу, учитывая уникальные обстоятельства и потребности наших клиентов. 
+                        Доверьтесь нашей команде профессионалов, и мы поможем вам найти оптимальное решение вашей ситуации.
+                    </p>
+                    <p class="text-left">
+                        Посмотрите примеры нашей работы — судебные решения, апелляционные, 
+                        кассационные и надзорные определения и составленные нами жалобы.
+                    </p>
+                    <a href="#" class="mt-4 inline-block text-blue-500">Узнать больше ></a>
+                </div>
+                <div class="ml-4 flex-none relative"> 
+                    <div class="absolute bg-white p-2 border border-black rounded-lg -translate-x-6 translate-y-2">
+                        <p>Локальные проблемы</p>
+                        <p>Локальные проблемы</p>
+                        <p>Локальные проблемы</p>
+                    </div>
+                    <div class="border border-black rounded-lg">
+                        <img src="~/public/5186391.png" alt="img" class="h-96 w-52 object-cover" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex items-stretch">
+                <div class="ml-4 flex-none relative"> 
+                    <div>
+                        <img src="~/public/5075562.jpg" alt="img" class="h-96 w-96 object-cover" />
+                    </div>
+                </div>
+                <div class="mb-4 flex-1 max-w-md">
+                    <h1 class="text-xl font-bold mb-2 text-center">Споры о наследовании:</h1>
+                    
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            дела об установлении <strong>факта принятия наследства</strong>
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"></div>
+
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            восстановление <strong>срока на принятие</strong> наследства
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"></div> 
+
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            споры о признании <strong>завещаний</strong> недействительными
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"></div>
+
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            дела об установлении <strong>факта принятия наследства.</strong>
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"></div>
+                    <a href="#" class="mt-4 inline-block text-blue-500">Узнать больше ></a>
+                </div>
+            </div>
+
+
+            <div class="flex items-stretch">
+                <div class="mb-4 flex-1 max-w-md">
+                    <h1 class="text-xl font-bold mb-2 text-center">Споры о браке, детях, имуществе:</h1>
+                    
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            дела о признании брака недействительным
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"></div>
+
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            споры об определении порядка пользования общим имуществом
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"> </div> 
+
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            споры о признании завещаний недействительными
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"></div>
+
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            дела об установлении факта принятия наследства.
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"></div>
+
+                    <div class="flex items-center mb-2">
+                        <img src="~/public/free-icon-checkmark-.png" alt="img" class="w-4 h-4 mr-2"/>
+                        <p class="text-left">
+                            восстановление срока на принятие наследства
+                        </p>
+                    </div>
+                    <div class="w-full h-0.5 bg-gray-300 mb-2"></div>
+                    <a href="#" class="mt-4 inline-block text-blue-500">Узнать больше ></a>
+                </div>
+
+                <div class="ml-4 flex-none relative"> 
+                    <div>
+                        <img src="~/public/5075562.jpg" alt="img" class="h-96 w-96 object-cover" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="flex items-stretch">
+                <div class="mb-4 flex-1 max-w-md">
+                    <h1 class="text-xl font-bold mb-2 text-left">Заказать иск об определением порядка общения с ребенком</h1>
+                    <p class="mb-2 text-left">
+                        Наши юристы готовы помочь вам заполучить готовые документы. Мы ищем индивидуальные подходы к каждому клиенту и делаем все для него.
+                    </p>
+                        <button class="border border-black rounded-lg text-center px-4">Заказать документ</button>
+                        <button class="border border-black rounded-lg text-center px-4">Купить готовый шаблон</button>
+                </div>
+                <div class="ml-4 flex-none relative"> 
+                    <div>
+                        <img src="~/public/20943399.png" alt="img" class="h-52 w-52 object-cover" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex justify-center">
+                <div class="flex-none w-1/4 flex items-center justify-center bg-blue-500 mx-2" style="min-height: 200px;">
+                    <div class="bg-white border border-black p-4">
+                        <img src="" alt="img" class="h-24 w-24"/>
+                        <p>Адрес офиса:</p>
+                        <p>Ул.Пушкина д.42 ...</p>
+                        <p>Телефон:</p>
+                        <p>8 (900) 000 00-00</p>
+                        <p>Почта:</p>
+                        <p>mail@gmail.com</p>
+                    </div>
+                </div>
+
+                <div class="flex-none w-1/4 flex items-center justify-center bg-green-500 mx-2" style="min-height: 200px;">
+                    <div class="bg-white border border-black p-4">
+                        <h1 class="text-center">Есть вопросы ? Напишите нам.</h1>
+
+                        <form>
+                            <div class="mb-4">
+                                <label for="email" class="block">Ваша почта:</label>
+                                <input type="email" id="email" class="border border-black text-black bg-white p-2 w-full" required>
+                            </div>
+                            <div class="mb-4">
+                                <label for="name" class="block">Имя:</label>
+                                <input type="text" id="name" class="border border-black text-black bg-white p-2 w-full" required>
+                            </div>
+                            <div class="mb-4">
+                                <label for="comment" class="block">Комментарий:</label>
+                                <textarea id="comment" class="border border-black text-black bg-white p-2 w-full" rows="4" required></textarea>
+                            </div>
+                            <button type="submit" class=" text-black bg-white border border-black items-start p-2 w-full">Отправить</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+    </div>
+</template>
+
