f8e99cf8a2f82e26def1791c050e6985c98ee256
Your Name <you@example.com>
2025-01-09 16:11:03 +0500

    Lending4

---
+++ pages/Leading4/index.vue
@@ -0,0 +1,48 @@
+<script></script>
+
+<template>
+  <main>
+    <div class="flex px-52">
+      <div class="flex-1">
+        <div class="items-center">
+          <img
+            src="~public/Pngtreecartoon_learn.png"
+            alt="Главная"
+            class="w-96 h-96"
+          />
+        </div>
+      </div>
+      <div class="flex-1">
+        <div class="items-end">
+          <h1 class="text-xl text-right items-end">
+            НАСЛЕДСТВЕННЫЕ ДЕЛА И СПОРЫ
+          </h1>
+          <h3 class="text-lg text-right items-end">- УПУЩЕНЫЕ СРОКИ.</h3>
+        </div>
+        <div>
+          <p class="text-left pr-52">
+            Упущенные сроки в наследственных делах могут привести к серьезным
+            последствиям, включая потерю прав на наследство. Наша задача —
+            помочь вам разобраться в этой непростой ситуации и восстановить ваши
+            права.
+          </p>
+          <br />
+          <p class="text-left ml-6 pr-48">
+            Не позволяйте упущенным срокам стать преградой на пути к вашему
+            наследству. Обратитесь к нам за помощью, и мы сделаем все возможное,
+            чтобы защитить ваши интересы и восстановить справедливость в
+            наследственных делах. Наша команда профессионалов готова поддержать
+            вас на каждом этапе этого процесса.
+          </p>
+        </div>
+        <button
+          class="bg-white border-2 border-black p-4 rounded-full font-bold mt-6 ml-16"
+        >
+          Бесплатная консультация
+        </button>
+      </div>
+    </div>
+
+    <div>123</div>
+  </main>
+</template>
--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <header></header>
+  <header class="bg-gray-900 text-white py-4">
+    <div class="max-w-6xl mx-auto h-c px-4 flex justify-between items-center">
+      <div class="text-xl font-bold">
+        <a href="#" class="hover:text-gray-300"
+          >Услуги юриста переводов жилого помещения в нежилое и наоборот</a
+        >
+      </div>
+      <nav class="space-x-6">
+        <a href="#" class="hover:text-gray-300">Главная</a>
+        <a href="#" class="hover:text-gray-300">Услуги</a>
+        <a href="#" class="hover:text-gray-300">О нас</a>
+        <a href="#" class="hover:text-gray-300">Контакты</a>
+      </nav>
+    </div>
+  </header>
 
   <main>
     <div class="p-6">
-      <div class="flex flex-col md:flex-row items-start px-4 md:px-52 mb-6">
+      <div
+        class="flex flex-col h-screen md:flex-row items-start px-4 md:px-52 mb-6"
+      >
         <div class="mb-4 flex flex-col items-center"></div>
         <div class="ml-0 md:ml-4 flex-1">
           <h1
@@ -172,30 +188,41 @@
       </div>
     </div>
 
-    <div>
-      <div class="flex items-center px-20">
-        <div class="flex-1">
+    <!-- <div>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="items-center">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+      </div>
+    </div> -->
+
+    <div class="flex content-evenly w-full justify-evenly">
+      <div class="flex justify-center">
+        <div class="">
           <img
             src="~/public/serious-colleagues-o.jpg"
             alt=""
             class="border-2 border-black w-72 h-96"
           />
         </div>
-        <div class="flex-1">
+        <div class="">
           <img
             src="~/public/serious-colleagues-o.jpg"
             alt=""
             class="border-2 border-black w-72 h-96"
           />
         </div>
-        <div class="flex-1">
+        <div class="">
           <img
             src="~/public/serious-colleagues-o.jpg"
             alt=""
             class="border-2 border-black w-72 h-96"
           />
         </div>
-        <div class="flex-1">
+        <div class="">
           <img
             src="~/public/serious-colleagues-o.jpg"
             alt=""
@@ -203,48 +230,228 @@
           />
         </div>
       </div>
-
+    </div>
+    <div>
       <div>
-        <h1 class="font-bold text-2xl">Схема сотрудничества:</h1>
-        <div class="border-2 border-black">
-          <div>
-            <img src="~/public/chad.png" alt="" class="" />
-            <div class="flex">
-              <div class="flex-1">
-                <div class="border-2 border-black rounded-lg">
-                  <div class="text-center justify-center items-center">
-                    <h1 class="text-center">Звонок Юристу</h1>
+        <div class="p-4">
+          <!-- <h1 class="font-bold text-2xl text-start bg-amber-100 px-44">
+            Схема сотрудничества:
+          </h1> -->
+          <div class="relative px-44">
+            <img
+              src="~/public/chad.png"
+              alt="ПодГлавный"
+              class="w-full h-[600px] mb-4"
+            />
+            <div
+              class="absolute top-0 left-0 right-0 bottom-0 flex flex-wrap justify-center items-stretch mt-6 px-96"
+            >
+              <div class="flex-1 mx-2 mb-4 h-96">
+                <div
+                  class="border-2 border-black rounded-3xl bg-white p-4 h-96 flex flex-col"
+                >
+                  <div class="text-center flex-grow">
+                    <h2 class="font-semibold text-xl">Звонок Юристу</h2>
                     <img
                       src="~/public/call2.png"
                       alt="Телефон"
-                      class="w-10 h-10 object-cover"
+                      class="w-20 h-20 object-cover mx-auto my-2"
                     />
-                    <p>
+                    <p class="text-lg px-2">
                       Краткий анализ Вашей проблемы и поиск путей решения,
                       назначение личной встречи.
                     </p>
+                    <a href="#" class="mt-4 inline-block text-blue-500 mb-2"
+                      >Узнать больше ></a
+                    >
                   </div>
                 </div>
               </div>
-              <div class="flex-1">
-                <div class="border-2 border-black rounded-lg">
-                  <div class="text-center justify-center items-center">
-                    <h1 class="text-center">встреча с юристом</h1>
+              <div class="flex-1 mx-2 mb-4 h-96">
+                <div
+                  class="border-2 border-black bg-white rounded-3xl p-4 h-96 flex flex-col"
+                >
+                  <div class="text-center flex-grow">
+                    <h2 class="font-semibold text-xl">Встреча с юристом</h2>
                     <img
                       src="~/public/user3.png"
                       alt="Телефон"
-                      class="w-10 h-10 object-cover"
+                      class="w-20 h-20 object-cover mx-auto my-2 mb-2"
                     />
-                    <p>
+                    <p class="text-lg px-2">
                       Первичная бесплатная консультация по ЖКХ, анализ
                       перспективы судебного дела, изучение документов клиента и
                       выработка судебной стратегии.
                     </p>
+                    <a href="#" class="mt-4 inline-block text-blue-500 mb-2"
+                      >Узнать больше ></a
+                    >
                   </div>
                 </div>
               </div>
-              <div class="flex-1">3</div>
-              <div class="flex-1">4</div>
+              <div class="flex-1 mx-2 mb-4 h-96">
+                <div
+                  class="border-2 border-black bg-white rounded-3xl p-4 h-96 flex flex-col"
+                >
+                  <div class="text-center flex-grow">
+                    <h2 class="font-semibold text-xl">Подписание договора</h2>
+                    <img
+                      src="~/public/pngwingcom_3.png"
+                      alt="Телефон"
+                      class="w-20s h-20 object-cover mx-auto my-2"
+                    />
+                    <p class="text-lg px-2">
+                      Подписание договора на оказание юридических услуг.
+                    </p>
+                    <a href="#" class="mt-4 inline-block text-blue-500 mb-2"
+                      >Узнать больше ></a
+                    >
+                  </div>
+                </div>
+              </div>
+              <div class="flex-1 mx-2 mb-4 h-96">
+                <div
+                  class="border-2 border-black bg-white rounded-3xl p-4 h-96 flex flex-col"
+                >
+                  <div class="text-center flex-grow">
+                    <h2 class="font-semibold text-xl">Ведение дела</h2>
+                    <img
+                      src="~/public/feedb.png"
+                      alt="Телефон"
+                      class="w-20 h-20 object-cover mx-auto my-2"
+                    />
+                    <p class="text-lg px-2">
+                      Исполнение обязательств по договору на оказание
+                      юридических услуг вплоть до получения желаемого
+                      результата.
+                    </p>
+                    <a href="#" class="mt-4 inline-block text-blue-500 mb-2"
+                      >Узнать больше ></a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <div class="container mx-auto p-4">
+        <div class="flex flex-col md:flex-row">
+          <div class="flex-1 md:mr-4 mb-4 md:mb-0">
+            <img
+              src="~/public/dialogue_with_one_pe.png"
+              alt="Диалог"
+              class="h-96 w-96 object-cover rounded-lg"
+            />
+            <div class="bg-orange-100 rounded-lg p-4 mt-4">
+              <h2 class="font-bold">Наши контакты:</h2>
+              <hr class="border-t border-black my-4 w-full" />
+            </div>
+          </div>
+          <div class="flex-1 w-96">
+            <div
+              class="border-2 border-black rounded-full font-bold p-4 mb-4 text-center"
+            >
+              Есть вопросы? Напиши нам.
+            </div>
+            <form>
+              <div class="mb-4">
+                <label for="email" class="block">Ваша почта:</label>
+                <input
+                  type="email"
+                  id="email"
+                  class="border border-black text-black bg-white p-2 w-full rounded"
+                  required
+                />
+              </div>
+              <div class="mb-4">
+                <label for="name" class="block">Имя:</label>
+                <input
+                  type="text"
+                  id="name"
+                  class="border border-black text-black bg-white p-2 w-full rounded"
+                  required
+                />
+              </div>
+              <div class="mb-4">
+                <label for="comment" class="block">Комментарий:</label>
+                <textarea
+                  id="comment"
+                  class="border border-black text-black bg-white p-2 w-full rounded"
+                  rows="4"
+                  required
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                class="text-black bg-white border border-black rounded-full p-2 w-full hover:bg-gray-300 transition"
+              >
+                Отправить
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-center items-center space-x-8 mt-4">
+        <div class="w-52">
+          <div class="relative">
+            <div
+              class="absolute inset-0 bg-gray-300 rounded-3xl shadow-lg transform translate-x-2 -translate-y-2 w-52 h-32"
+            ></div>
+            <div
+              class="border-2 bg-orange-100 rounded-3xl w-52 h-32 relative z-10"
+            >
+              <div class="flex items-center pl-2">
+                <img src="~/public/call2.png" alt="Телефон" class="w-10 h-10" />
+                <h1 class="pl-2">Позвоните нам</h1>
+              </div>
+              <p>+7 (900) 000 00-00</p>
+              <p>+7 (900) 000 00-00</p>
+            </div>
+          </div>
+        </div>
+        <div class="w-52">
+          <div class="relative">
+            <div
+              class="absolute inset-0 bg-gray-300 rounded-3xl shadow-lg transform translate-x-2 -translate-y-2 w-52 h-32"
+            ></div>
+            <div
+              class="border-2 bg-orange-100 rounded-3xl w-52 h-32 relative z-10"
+            >
+              <div class="flex items-center pl-2">
+                <img
+                  src="~/public/placeholder.png"
+                  alt="Местоположение"
+                  class="w-10 h-10"
+                />
+                <h1 class="pl-2">Местоположение</h1>
+              </div>
+              <p>Ул.Пушкина д.42</p>
+              <p>Ул.Пушкина д.42</p>
+            </div>
+          </div>
+        </div>
+        <div class="w-52">
+          <div class="relative">
+            <div
+              class="absolute inset-0 bg-gray-300 rounded-3xl shadow-lg transform translate-x-2 -translate-y-2 w-52 h-32"
+            ></div>
+            <div
+              class="border-2 bg-orange-100 rounded-3xl w-52 h-32 relative z-10"
+            >
+              <div class="flex items-center pl-2">
+                <img
+                  src="~/public/pngwingcom_4.png"
+                  alt="Время работы"
+                  class="w-10 h-10"
+                />
+                <h1 class="pl-2">Время работы</h1>
+              </div>
+              <p>ПН-ПТ: 8:00-18:00</p>
+              <p>СБ-ВС: 9:00-17:00</p>
             </div>
           </div>
         </div>
@@ -252,5 +459,26 @@
     </div>
   </main>
 
-  <footer></footer>
+  <footer class="bg-gray-800 text-white py-6 mt-6">
+    <div class="max-w-4xl mx-auto px-4">
+      <div class="flex justify-between items-center">
+        <div>
+          <h2 class="text-lg font-bold">Контакты</h2>
+          <div>Телефон: +8 (900) 000 00-00</div>
+          <div>Email: gmail@gmail.com</div>
+        </div>
+        <div>
+          <h2 class="text-lg font-bold">Социальные сети</h2>
+          <div class="flex space-x-4">
+            <a href="#" class="text-gray-400 hover:text-white">1</a>
+            <a href="#" class="text-gray-400 hover:text-white">2</a>
+            <a href="#" class="text-gray-400 hover:text-white">3</a>
+          </div>
+        </div>
+      </div>
+      <div class="mt-6 border-t border-gray-700 pt-4 text-center">
+        <div class="text-sm">© 2024 Ваша компания. Все права защищены.</div>
+      </div>
+    </div>
+  </footer>
 </template>
